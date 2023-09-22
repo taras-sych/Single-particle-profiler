8490d908879a7eb444274d7093081e887b97fe91
Taras Sych <36703672+taras-sych@users.noreply.github.com>
2023-09-22 22:40:26 +0200

    123

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 setup(name='fluct_prof',
       version='1.0',
       description='Fluctuometry profiler',
-      install_requires=['tk','matplotlib','lmfit', 'ttkwidgets', 'scipy', 'seaborn'],
+      install_requires=['tk','matplotlib','lmfit', 'ttkwidgets', 'scipy', 'seaborn', 'tifffile'],
       long_description = readme(),
       url='',
       packages = find_packages(),
